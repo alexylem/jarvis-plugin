56f630b7d6765a8e8f2ec2437b2efcb8930f50eb
Kévin <kevin.caradant@gmail.com>
2016-10-10 23:40:29 +0200

    Update functions.sh

--- en/functions.sh
+++ en/functions.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
+callUrlKodiEn (){
+   curl -s -H "Content-Type: application/json" -X POST -d ${1} http://$kodiIpEn:$kodiPortEn/jsonrpc > /dev/null
+}
