1483e42cb2ac652629e5c595b2a94232b378816d
Alexandre Mély <alexandre.mely@gmail.com>
2017-01-08 16:21:47 +0100

    added plugin level functions.sh file

--- en/functions.sh
+++ en/functions.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# jv_pg_XX_myfunction () { }
-# jv for JarVis
-# pg for PluGin
-# XX can be a two letters code for your plugin, ex: ww for Weather Wunderground
+# Here you can define translations to be used in the plugin functions file
+# the below code is an sample to be reused:
+# 1) uncomment to function below
+# 2) replace XXX by your plugin name (short)
+# 3) remove and add your own translations
+# 4) you can the arguments $2, $3 passed to this function
+# 5) in your plugin functions.sh file, use it like this:
+#      say "$(pv_myplugin_lang the_answer_is "oui")"
+#      => Jarvis: La réponse est oui
+
+#pv_XXX_lang () {
+#    case "$1" in
+#        i_check) echo "Je regarde...";;
+#        the_answer_is) echo "La réponse est $2";;
+#    esac
+#} 
