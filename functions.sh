bafbee6616c810a7b5798965e6671ab83488dd86
Pascal Obstetar <pascal.obstetar@gmail.com>
2017-01-17 11:30:38 +0100

    Init plugin

--- functions.sh
+++ functions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Here you can create functions which will be available from the commands file
 # You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
+
+jv_pg_ap_search ()
+{
+
+#room definition
+local APPEAR_IN_QUERY="https://appear.in/$appear_room"
+
+#open defaut web browser with the appear.in room
+nohup chromium-browser %U $APPEAR_IN_QUERY
+
+}
+
